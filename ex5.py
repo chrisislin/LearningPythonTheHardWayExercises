b919050e7dc69ea391d297060affd8d36b86da42
chrisislin <christopherlin@umail.ucsb.edu>
2018-06-17 11:26:05 -0700

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,16 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74 * 2.54
+weight = 180 * .454
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %r." % name
+print "He's %d centimeters tall." % height
+print "He's %d kilograms heavy." %weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair."%(eyes, hair)
+print "His teeth are usually %s depending on the coffee." %(teeth)
+
+print "If I add %d, %d, and %d I get %d." %(age, height, weight, age + height + weight)
