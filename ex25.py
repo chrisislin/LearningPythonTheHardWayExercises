87e79ec4264e7b0c2dbdf13d45e4201b1ffc47e0
chrisislin <christopherlin@umail.ucsb.edu>
2018-06-18 23:34:10 -0700

    Add files via upload

---
+++ ex25.py
@@ -0,0 +1,44 @@
+def break_words(stuff):
+	"""This function will break up words"""
+	words = stuff.split(' ')
+	return words
+
+def sort_words(words):
+	""" sorts the words. """
+	return sorted(words)
+
+def print_first_word(words):
+	"""prints the first word after taking it off"""
+	word = words.pop(0)
+	print word
+
+def print_last_word(words):
+	"""prints last word after taking it off"""
+	word = words.pop(-1)
+	print word
+
+def sort_sentence(sentence):
+	"""full sentence and returns sorted"""
+	words = break_words(sentence)
+	return sort_words(words)
+
+def print_first_and_last(sentence):
+	"""prints final and last words of sentence"""
+	words = break_words(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+	"""sorts the words then prints the first and last one"""
+	words = sort_sentence(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+	"""sorts the words then prints first and last"""
+	words = sort_sentence(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+
+
